6c2e8a3e64cb5794935a97f96850c70d725a603d
Emma Boardman <emma@emmaboardman.co.uk>
2016-10-23 09:58:32 +0100

    catching up on last weeks homework

---
+++ client/src/components/Song.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Song = function(props){
+
+   return(
+    <div>
+    <img src={props.image}/>
+    <div className="details">
+      <h3>{props.position}. {props.tite}</h3>
+      <h4>{props.artist}</h4>
+    </div>
+    </div>
+    )
+};
+
+module.exports = Song;
